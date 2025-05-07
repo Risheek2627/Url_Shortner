0f04f95f401f5cf95d0d889dd21f74273f618095
Risheek <risheek2627.com>
2025-05-07 14:33:16 +0530

    Added static serve setup and frontend build

--- server/index.js
+++ server/index.js
@@ -6,10 +6,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
